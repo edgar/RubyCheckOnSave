ef28f096ca03306bd7a173fa8f0092c40c639637
Sergey Pchelincev <jalkoby91@gmail.com>
2014-01-20 23:34:04 +0200

    Show popup on save and update documentation
    Show popup window instead opening ST console;
    Update documentation about usage plugin;

--- RubyCheckOnSave.py
+++ RubyCheckOnSave.py
@@ -1,4 +1,4 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, subprocess
 
 class RubyCheckOnSave(sublime_plugin.EventListener):
     def on_post_save(self, view):
@@ -11,4 +11,14 @@ class RubyCheckOnSaveCommand(sublime_plugin.TextCommand):
         global_settings = sublime.load_settings(__name__ + '.sublime-settings')
         cmd_setting = 'ruby_check_on_save_cmd'
         ruby = view.settings().get(cmd_setting, global_settings.get(cmd_setting))
-        view.window().run_command("exec", {"cmd": [ruby, "-cw", view.file_name()]})
+        args = [ruby, "-cw", "%s" % view.file_name()]
+        child_process = subprocess.Popen(args, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+        cmd_output = child_process.communicate()[0]
+
+        if child_process.returncode != 0:
+            first_line, desc = cmd_output.split('\n', 1)
+            desc = desc.split('\n', 1)[0]
+            _, line, error = first_line.split(':', 2)
+            err_message = "Ruby syntax error at %(line)s line\n\n%(desc)s\n\n%(error)s" % { "line": line, "desc": desc, "error": error }
+            sublime.error_message(err_message)
+            view.window().run_command("goto_line", {"line": line} )
